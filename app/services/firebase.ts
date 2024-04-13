311ec5aa697e399504f6ce80d86d024b97f31ae5
yui666a <haiso666@gmail.com>
2024-04-13 22:04:22 +0900

    fix: firebaseConfig

--- app/services/firebase.ts
+++ app/services/firebase.ts
@@ -1,12 +1,11 @@
 import { initializeApp } from 'firebase/app'
 
 const firebaseConfig = {
-  apiKey: 'AIzaSyCTNcpaBcn8bzjDGhWVpaQ6YOnZzHECwUE',
-  authDomain: 'gm-spot-saver.web.app',
+  apiKey: 'AIzaSyBrgknhksK3t6dtVzjUBl_pSaG4JmjmrgE',
+  authDomain: 'gm-spot-saver.firebaseapp.com',
   projectId: 'gm-spot-saver',
   storageBucket: 'gm-spot-saver.appspot.com',
-  messagingSenderId: '555137498198',
-  appId: '1:555137498198:web:380bd830ee347237890ff7',
-}
-
+  messagingSenderId: '287487504882',
+  appId: '1:287487504882:web:b3078ebafdc8e7a6a27b6b'
+};
 export const app = initializeApp(firebaseConfig)
